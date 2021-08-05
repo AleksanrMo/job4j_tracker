732ed11a47d7771ca7904adda7c7a0cfcd401588
AleksandrMo <sushanta@mail.ru>
2021-08-05 17:35:02 +0700

    исправил код

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -3,30 +3,10 @@ package ru.job4j.pojo;
 public class ShopDrop {
 
     public static Product[] delete(Product[] products, int index) {
-        for (int i = 0; i < products.length; i++) {
-            if (i == index) {
-               products[i] = null;
-               break;
-            }
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
         }
-        for (int i = 0; i < products.length; i++) {
-            if (products[i] != null && products[0] == null) {
-               products[0] = products[i];
-               products[i] = null;
-               break;
-            }
-        }
-        for (int i = 1; i < products.length; i++) {
-            if (products[i] == null) {
-                for (int j = i + 1; j < products.length; j++) {
-                    if (products[j] != null) {
-                        products[i] = products[j];
-                        products[j] = null;
-                        break;
-                    }
-                }
-            }
-        }
+        products[products.length - 1] = null;
         return products;
     }
 }
