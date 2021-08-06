70e25f2ce406be848dfb3c25f55f3011f9c2a5f1
AleksandrMo <sushanta@mail.ru>
2021-08-06 13:52:46 +0700

    сократил код

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -48,16 +48,13 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        Item[] test = new Item[size];
         int sum = 0;
         for (int i = 0; i < size; i++) {
             if (items[i].getName().equals(key)) {
-                test[sum] = items[i];
                 sum++;
             }
         }
-        test =  Arrays.copyOf(items, sum);
-        return test;
+        return  Arrays.copyOf(items, sum);
     }
 
 }
