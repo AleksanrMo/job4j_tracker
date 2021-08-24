531a48a0dd95297ce47c061ad03c9ba78a16492f
AleksandrMo <sushanta@mail.ru>
2021-08-24 20:01:17 +0700

    добавил Task

---
+++ src/main/java/ru/job4j/collaction/Task.java
@@ -0,0 +1,17 @@
+package ru.job4j.collaction;
+import java.util.HashSet;
+import java.util.List;
+public class Task {
+
+    private String number;
+    private String description;
+
+    public Task(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
