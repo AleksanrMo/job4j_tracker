45ee1731870d6aa21ced19d59d10a37ae99a6fa3
AleksandrMo <sushanta@mail.ru>
2021-08-27 11:01:44 +0700

    добавил тест

--- src/test/java/ru/job4j/collaction/OrderConvertTest.java
+++ src/test/java/ru/job4j/collaction/OrderConvertTest.java
@@ -17,4 +17,18 @@ public class OrderConvertTest {
         HashMap<String, Order> map = OrderConvert.process(orders);
         assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
     }
+
+    @Test
+    public void whenAddDuplicateOrder() {
+        List<Order> orders = new ArrayList<>();
+        Order one = new Order("12", "Table");
+        Order two = new Order("13", "Stool");
+        Order three = new Order("12", "Pan");
+        orders.add(one);
+        orders.add(two);
+        orders.add(three);
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.size(), is(2));
+
+    }
 }
