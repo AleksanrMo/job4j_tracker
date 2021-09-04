051b55df64c45c14b5d28ba0590997c73570f50e
AleksandrMo <sushanta@mail.ru>
2021-09-04 12:45:36 +0700

    1.3. Встроенные функциональные интерфейсыный интерфейс

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,15 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println("compare - " + left.length() + " : " + right.length());
+            return right.length() - left.length();
+        };
+        String a = "привет!";
+        String b = "пока!";
+        System.out.println(comparator.compare(a, b));
+    }
+}
