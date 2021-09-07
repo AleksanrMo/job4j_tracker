aa05c985f70a11853daf98e8ca939cd8d9380600
AleksandrMo <sushanta@mail.ru>
2021-09-07 15:37:08 +0700

    2. Список адресов

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.stream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class ProfilesTest {
+
+    @Test
+    public void whenAddElement() {
+        Profiles test = new Profiles();
+        List<Profile> puzzle = Arrays.asList(new Profile(
+                new Address("Barnaul", "Lenina", 43, 54)));
+        List<Address> expected = List.of(new Address(
+                "Barnaul", "Lenina", 43, 54));
+        assertThat(test.collect(puzzle), is(expected));
+    }
+
+    @Test
+    public void whenAdd2Elements() {
+        Profiles test = new Profiles();
+        List<Profile> list = Arrays.asList(new Profile(
+                new Address("Barnaul", "Lenina", 43, 54)),
+               new Profile(new Address("Novosibirsk", "Pushkina", 12, 128)));
+        List<Address> expected = List.of(new Address(
+                "Barnaul", "Lenina", 43, 54),
+                new Address("Novosibirsk", "Pushkina", 12, 128));
+        assertThat(test.collect(list), is(expected));
+    }
+}
