6c02d88e508fb72a703f1f524ddecf0578a3b8b9
AleksandrMo <sushanta@mail.ru>
2021-09-09 11:47:50 +0700

    Stream API улучшения

--- src/main/java/ru/job4j/stream/Student.java
+++ src/main/java/ru/job4j/stream/Student.java
@@ -5,10 +5,14 @@ import java.util.Objects;
 public class Student {
 
     private int score;
-
     private String surname;
 
     public Student(int score, String surname) {
+        this.score = score;
+        this.surname = surname;
+    }
+
+    public Student(String surname, int score) {
         this.score = score;
         this.surname = surname;
     }
