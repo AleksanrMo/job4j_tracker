c60cb7beede91a6c848fe6824c556507c4620f6c
AleksandrMo <sushanta@mail.ru>
2021-09-13 18:00:52 +0700

    Аттестация

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,51 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+
+    public static double averageScore(Stream<Pupil> stream) {
+       return stream.flatMap(p -> p.getSubjects()
+               .stream()).mapToInt(Subject::getScore)
+               .average().orElse(0D);
+
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.map(e -> new Tuple(e.getName(), e.getSubjects()
+                .stream().mapToInt(Subject::getScore)
+                .average().orElse(0D)))
+                .collect(Collectors.toList());
+
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+
+        Map<String, Double> map = stream.flatMap(sub -> sub.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName,
+                        Collectors.averagingDouble(Subject::getScore)));
+        return map.entrySet().stream().map(e -> new Tuple(e.getKey(), e.getValue()))
+                .sorted(Comparator.comparing(Tuple::getName).reversed())
+                .collect(Collectors.toList());
+
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+       return  stream.map(e -> new Tuple(e.getName(),
+                e.getSubjects().stream().mapToInt(Subject::getScore).sum()))
+               .max(Comparator.comparing(Tuple::getScore)).orElse(null);
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        Map<String, Double> map = stream.flatMap(sub -> sub.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName,
+                        Collectors.summingDouble(Subject::getScore)));
+       return  map.entrySet().stream().map(e -> new Tuple(e.getKey(), e.getValue()))
+                .max(Comparator.comparing(Tuple::getScore)).orElse(null);
+    }
+}
